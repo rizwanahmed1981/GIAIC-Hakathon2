5cc9ffa98a106aae91e71470344b67d332a5edc4
rizwanahmed1981 <109456571+rizwanahmed1981@users.noreply.github.com>
2024-12-08 18:50:15 +0500

    topbar completed

---
+++ src/app/components/topbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Image from 'next/image'
+import call from "@/public/call.svg"
+import email from "@/public/email.svg"
+import insta from "@/public/insta.svg"
+import youtube from "@/public/youtube.svg"
+import fb from "@/public/fb.svg"
+import twitter from "@/public/twitter.svg"
+
+
+const Topbar = () => {
+    return (
+        <div className='max-w-full mx-auto p-6 flex flex-row item-center justify-between bg-[#252b42] text-white font-bold text-[14px]'>
+            <div className='max-w-7xl  flex flex-row gap-6'>
+                <div className='flex flex-row gap-2'>
+                    <Image src={call} alt="call icon" width={16} height={16} />
+                    <p>+92(312) 03-06090</p>
+                </div>
+                <div className='flex flex-row gap-2'>
+                    <Image src={email} alt="email icon" width={16} height={16} />
+                    <p>razykan.ra@gmail.com</p>
+                </div>
+            </div>
+
+
+            <div className='max-w-7xl '>
+                <p>Follow Us  and get a chance to win 80% off</p>
+            </div>
+
+            <div className='max-w-7xl  flex flex-row gap-6'>
+                <p>Follow US :</p>
+                <div className='flex flex-row gap-2'>
+                <Image src={insta} alt="instagram icon" width={26} height={26} />
+                <Image src={youtube} alt="youtube icon" width={26} height={26} />
+                <Image src={fb} alt="facebook icon" width={26} height={26} />
+                <Image src={twitter} alt="twitter icon" width={26} height={26} />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Topbar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Main } from "next/document";
+import Topbar from "./components/topbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Topbar/>
+        <main>
         {children}
+        </main>
       </body>
     </html>
   );
