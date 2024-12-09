80e3583132515c0dd0342104a6f64b0bef19805c
rizwanahmed1981 <109456571+rizwanahmed1981@users.noreply.github.com>
2024-12-09 21:05:06 +0500

    blog section added

--- src/app/components/blog.tsx
+++ src/app/components/blog.tsx
@@ -1,18 +1,54 @@
 import React from 'react'
+import Image from 'next/image'
+import BlogCard from './blogcard'
 
 const Blog = () => {
   return (
-    <div className='md:mt-20'>
-        <div className='flex flex-col text-center items-center md:h-[1044px] md:w-full gap-4'>
-            <p className='text-[#23a6f0] text-[14px] font-bold'>Practice Advice</p>
-            <h1 className='font-bold text-[40px] text-[#252b42]'>Featured Posts</h1>
-            <p className='text-[14px] font-normal text-[@737373] opacity-100'>Problems trying to resolve the conflict between <br/>
-            the two major realms of Classical physics: Newtonian mechanics</p>
-        </div>
+
+    <div className='flex flex-col gap-10 my-10 md:mt-20 md:h-[1044px] md:w-full'>
+      <div className='flex flex-col text-center items-center  gap-4'>
+        <p className='text-[#23a6f0] text-[14px] font-bold'>Practice Advice</p>
+        <h1 className='font-bold text-[40px] text-[#252b42]'>Featured Posts</h1>
+        <p className='text-[14px] font-normal text-[@737373] opacity-100'>Problems trying to resolve the conflict between <br />
+          the two major realms of Classical physics: Newtonian mechanics</p>
+      </div>
 
 
-        <div></div>
+      <div className="w-full pl-10 md:ml-40 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-2 items-center justify-center">
+        {BlogCard.map((item, index) => {
+          return (
+            <div key={index} className='flex flex-col gap-2'>
+              <Image src={item.image[0]} alt='city clock image' width={348} height={300} />
+              <div className='flex flex-row items-start gap-4'>
+                <p className='font-normal text-[12px] text-[#8EC2F2] opacity-100'>{item.top[0]}</p>
+                <p className='font-normal text-[14px] text-[#737373] opacity-100'>{item.top[1]}</p>
+                <p className='font-normal text-[12px] text-[#737373] opacity-100'>{item.top[2]}</p>
+              </div>
+              <div className='flex flex-col items-start justify-evenly gap-4'>
+                <p className='font-normal text-[20px] text-[#252b42] opacity-100 text-wrap w-[247px] h-[60px]'>{item.title}</p>
+                <p className='w-[280px] h-[60px] text-[14px] font-normal text-wrap text-[#737373] opacity-100'>{item.discription}</p>
+              </div>
+              <div className='w-[298px] h-[46px] text-[12px] font-normal opacity-100 flex flex-row justify-between gap-6 md:ml-6 md:mr-'>
+                <div className='flex flex-row gap-2 items-center'>
+                  <Image src={item.image[1]} alt='clock pic'/>
+                  <p className='text-[#737373]'>{item.date}</p>
+                </div>
+                <div className='flex flex-row gap-2 items-center'>
+                  <Image src={item.image[2]} alt='graph pic'/>
+                  <p className='text-[#737373]'>{item.comments}</p>
+                </div>
+              </div>
+              <div className='flex flex-row gap-4 items-center' >
+                <p className='font-bold text-[14px] text-[#737373] opacity-100'>{item.learn}</p>
+                <Image src={item.image[3]} alt='arrow right facing' className='text-[#23A6F0]'/>
+              </div>
+
+            </div>
+          )
+        })}
+      </div>
     </div>
+
   )
 }
 
---
+++ src/app/components/blogcard.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import clock from "@/app/images/clock.svg";
+import graph from "@/app/images/graph.svg";
+import streetblock from "@/app/images/streetblock.png";
+import pinkcar from "@/app/images/pinkcar.png";
+import umbrailas from "@/app/images/umbrailas.png";
+import arrowright from "@/app/images/arrowright.svg";
+
+
+const BlogCard = [
+    {
+        image:[streetblock,clock,graph, arrowright],
+        top:["Google", "Trending", "New"],
+        title:"Loudest à la Madison #1 (L'integral)",
+        discription:"We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+        date:"22 April 2021",
+        comments: "10 Comments",
+        learn:"Learn More"
+    },
+    {
+        image:[pinkcar,clock,graph,arrowright],
+        top:["Google", "Trending", "New"],
+        title:"Loudest à la Madison #1 (L'integral)",
+        discription:"We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+        date:"22 April 2021",
+        comments: "10 Comments",
+        learn:"Learn More"
+    },
+    {
+        image:[umbrailas,clock,graph,arrowright],
+        top:["Google", "Trending", "New"],
+        title:"Loudest à la Madison #1 (L'integral)",
+        discription:"We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+        date:"22 April 2021",
+        comments: "10 Comments",
+        learn:"Learn More"
+    },
+]
+export default BlogCard
